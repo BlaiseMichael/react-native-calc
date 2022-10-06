52c480033415b4f9cda4af3595b5d44fb5e86dcd
BlaiseMichael <114963197+BlaiseMichael@users.noreply.github.com>
2022-10-06 06:07:05 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import CalculatorScreen from './Screens/CalculatorScreen';
+export default function App() {
+  return (
+    <CalculatorScreen/>
+  );
+}
+
+
